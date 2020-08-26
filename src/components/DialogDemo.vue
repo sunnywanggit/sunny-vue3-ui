ce56971e551f270ade64946c9223cbe1ea2e70cc
ergouwang <330035385@qq.com>
2020-08-26 22:17:19 +0800

    dialog 完成显示隐藏功能

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,3 +1,27 @@
 <template>
-  <div>Dialog 示例</div>
+    <SunnyDialog :visible="visible"/>
+    <Button @click="handleToggle">toggle</Button>
+    {{visible}}
+
 </template>
+
+<script>
+
+    import {ref} from 'vue'
+    import SunnyDialog from "../lib/SunnyDialog/SunnyDialog.vue";
+    import Button from "../lib/Button/Button.vue";
+    export default {
+        components: {Button, SunnyDialog},
+        setup(){
+            let visible = ref(false);
+            const handleToggle=()=>{
+                visible.value = !visible.value;
+            }
+            return {visible,handleToggle}
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
