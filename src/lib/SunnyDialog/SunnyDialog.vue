07e4bc7f11c14845abdfec35732f68470ab41d23
ergouwang <330035385@qq.com>
2020-08-26 22:00:37 +0800

    dialog 组件基本结构搭建完成

---
+++ src/lib/SunnyDialog/SunnyDialog.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="sui-dialog-overlay"></div>
+    <div class="sui-dialog-wrapper">
+        <header>标题</header>
+        <main>
+            <p>第一行字</p
+            <p>第二行字</p>
+        </main>
+        <footer>
+            <Button>OK</Button>
+            <Button>Cancel</Button>
+        </footer>
+    </div>
+</template>
+<script lang="ts">
+    import Button from "../Button/Button.vue";
+    export default {
+        name:"SunnyToast",
+        components: {Button}
+    }
+
+</script>
