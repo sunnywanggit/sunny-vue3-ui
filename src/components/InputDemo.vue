707ecbd6b98be6470f6871440ada90759598784d
ergouwang <330035385@qq.com>
2020-09-16 09:34:06 +0800

    完善 Input 组件文档编写

--- src/components/InputDemo.vue
+++ src/components/InputDemo.vue
@@ -1,26 +1,66 @@
 <template>
-    <h1>支持value</h1>
-    <Input value="xxx" />
-    <h1>支持 disabled</h1>
-    <Input value="xxx" disabled />
-    <h1>支持 readonly</h1>
-    <Input value="xxx"  readonly/>
-    <h1>支持 error</h1>
-    <Input value="xxx" error="error"/>
-    <h1>支持输入数字</h1>
-    <Input  type="number"/>
-    <h1>支持输入密码</h1>
-    <Input  type="password"/>
-    <h1>支持 placeholder</h1>
-    <Input placeholder="xxx"/>
+    <div>
+        <h2 class="margin20">Input 输入框</h2>
+        <h3 class="margin15">概述</h3>
+        <section class="margin10">
+            <p class="text">通过鼠标或键盘输入内容，是最基础的表单域的包装。</p>
+        </section>
+        <h3 class="margin15">代码示例</h3>
+
+        <section class="card">
+            <SunnyInput placeholder="basic usage" />
+            <h4 class="margin10">基本使用</h4>
+            <p class="text">基本使用</p>
+        </section>
+
+        <section class="card">
+            <SunnyInput placeholder="disabled" disabled/>
+            <h4 class="margin10">是否禁用</h4>
+            <p class="text">是否禁用状态，默认为 false</p>
+        </section>
+
+        <section class="card">
+            <SunnyInput placeholder="readonly" readonly/>
+            <h4 class="margin10">只读</h4>
+            <p class="text">设置 readonly 开启只读状态，默认为 false</p>
+        </section>
+
+        <section class="card">
+            <SunnyInput placeholder="error" value="error" error="error"/>
+            <h4 class="margin10">error</h4>
+            <p class="text">支持 error 提示</p>
+        </section>
+
+        <section class="card">
+            <SunnyInput size="large" placeholder="large size" style="margin-bottom:20px"/>
+            <br>
+            <SunnyInput placeholder="default size" style="margin-bottom:20px"/>
+            <br>
+            <SunnyInput size="small" placeholder="small size" />
+            <h4 class="margin10">三种大小</h4>
+            <p class="text">我们为 Input 输入框定义了三种尺寸（大、默认、小），高度分别为 40px、32px 和 24px。</p>
+        </section>
+
+        <section class="card">
+            <SunnyInput placeholder="input a number" />
+            <h4 class="margin10">输入时格式化展示</h4>
+            <p class="text">格式化输入内容，仅支支持数字输入</p>
+        </section>
+
+        <section class="card">
+            <SunnyInput placeholder="password" />
+            <h4 class="margin10">密码输入</h4>
+            <p class="text">设置 type="password" 即可支持密码输入</p>
+        </section>
+    </div>
 
 </template>
 
 <script>
-    import Input from '../lib/Input/Input.vue';
+    import SunnyInput from '../lib/Input/Input.vue';
     export default {
         name:"InputDemo",
-        components: {Input}
+        components: {SunnyInput}
     };
 </script>
 
--- src/lib/Input/Input.vue
+++ src/lib/Input/Input.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="sui-input" :class="{error}">
-        <input :value="value" :disabled="disabled" :readonly="readonly" :type="type" :placeholder="placeholder">
+        <input :value="value" :disabled="disabled" :readonly="readonly" :type="type" :placeholder="placeholder" :class="size">
         <template v-if="error">
             <span class="sui-input-errorMessage">{{error}}</span>
         </template>
@@ -8,14 +8,15 @@
 </template>
 <script>
     export default {
-        name: 'Input',
+        name: 'SunnyInput',
         props: {
             value: { type: String },
             disabled: { type: Boolean, default: false },
             readonly: { type: Boolean, default: false },
             error: { type: String },
             type:{type:String,default:'text'},
-            placeholder:{type:String}
+            placeholder:{type:String},
+            size:{type:String,default:'middle'}
         }
     }
 </script>
@@ -27,13 +28,24 @@
     $font-size: 12px;
     $box-shadow-color: rgba(0, 0, 0, 0.5);
     $red: #F1453D;
-    .sui-input{ font-size: $font-size; display: inline-flex;
+
+    .sui-input{ 
+        font-size: $font-size; 
+        display: inline-flex;
         align-items: center;
         > :not(:last-child) {margin-right: .5em; }
-        > input { height: 32px; border: 1px solid $border-color; border-radius: 4px; padding: 0 8px; font-size: inherit;
+        > input { 
+            height: 32px; 
+            border: 1px solid $border-color; 
+            border-radius: 4px; 
+            padding: 0 8px; 
+            font-size: inherit;
             &:hover { border-color: $border-color-hover; }
             &:focus { box-shadow: inset 0 1px 3px $box-shadow-color; outline: none; }
             &[disabled], &[readonly] {border-color: #bbb;color: #bbb;cursor: not-allowed; }
+            &.large{height: 40px;}
+            &.small{height: 24px;}
+            
         }
         &.error {
             > input { border-color: $red; }
