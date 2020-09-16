6a17c2b9bb55e19a94ea18d20bc1132155aa29df
ergouwang <330035385@qq.com>
2020-09-16 09:59:00 +0800

    解决问题：如何从父组件中得知子组件的类型

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
-    tabs
+    Tabs 组件
+    <component :is="defaults[0]"/>
+    <component :is="defaults[1]"/>
   </div>
 </template>
 
 <script lang="ts">
+import Tab from "./Tab.vue";
 export default {
-  name:"Tabs"
+  name:"Tabs",
+  setup(props,context){
+    const  defaults = context.slots.default();
+    //判断子组件类型是否正确，通过检查 defaults 的 type 来检查是不是我们要的组件类型
+    console.log(defaults[0].type === Tab);
+    return {
+      defaults
+    }
+  }
 
 }
 </script>
